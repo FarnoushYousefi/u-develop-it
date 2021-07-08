fb0e702ded9cc1b3d95371448e8a0404826de5ec
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-07-08 18:14:15 -0400

    add the routes

--- server.js
+++ server.js
@@ -1,42 +1,27 @@
 const express = require('express');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
 
-const inputCheck = require('./db/utils/inputCheck');
-const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const apiRoutes = require('./routes/apiRoutes');
-app.use('/api', apiRoutes);
-const db = require('./db/connection');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// Connect to database
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
 
 // Default response for any other request (Not Found)
-// app.use((req, res) => {
-//   res.status(404).end();
-// });
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//   console.log(rows);
-// });
+app.use((req, res) => {
+  res.status(404).end();
+});
 
-/// Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-// VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-// db.query(sql, params, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
-// Get all candidates
-
-// Create a candidate
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
